1d6dcd35df2a192c6a3d6ffbfb9ae435ec8d7025
dCOMMICS <sanwurld@gmail.com>
2024-08-16 23:46:20 +0300

    initial commit

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ console.log(c)
  */
 c.fillStyle = '#450a0a'
 c.fillRect(0,0,canvas.width,canvas.height)
+
+/**
+ * ? asteroids design
+ */
+
+class Player {
+    constructor (position, velocity) {
+        this.position = this.position   // {x: 0, y: 0}
+        this.velocity = this.velocity
+    }
+
+    // draw (){
+    //     c.fillStyle = '#f8fafc'
+    //     c.fillRect( this.position.x, this.position.y, 100,100)
+    // }
+    draw() {
+    c.fillStyle = '#f8fafc';
+    c.fillRect(this.position.x, this.position.y, 100, 100);
+}
+
+}
+
+// const player = new Player ( { {x: 0, y: 0},  {x: 0, y: 0} } )
+
+const player = new Player({ 
+    position: {x: 200, y: 100}, 
+    velocity: {x: 0, y: 0},
+ });
+
+ player.draw()
+
+console.log(player)
